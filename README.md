25940c0e26a91f307c5df878f9eab54fc941ef53
Ben Lee <Ben.m.lee@outlook.com>
2023-11-27 22:50:30 +1100

    [first draft]

--- README.md
+++ README.md
@@ -1,67 +1,13 @@
-# PixiDB
+# Session Manager
 
-A small all-in-memory database with background file writing.
+A session manager designed for sveltekit
 
 ## Basic Example
 ```
-// Types for saftey and completion
-interface User {
-  id: string;
-  age: number;
-}
 
-// Make an object that will house all your tables
-export const db = {
-  users: new Table<User>('/path/to/db/dir', 'users')
-}
-
-db.users.set({
-  id: 'cool_guy',
-  age: 42
-})
-let arr = db.users.getAll()
-let arr = db.users.get((v) => v.age > 10)
-let user = db.users.getOne('cool_guy')
 ```
 
-## Schemas
-You can define a schema for a table with the .schema().
 
-Doing so will enable validation checking on all .set() and .new() calls.
-```
-let users = new Table('users', null).schema({
-  id: 'string',
-  age: 'number',
-  verified: 'boolean',
-})
-
-Schema objects can also be nested like so:
-{
-  id: 'string',
-  age: 'number',
-  settings: {
-    darkmode: 'boolean',
-    volume: 'number'
-  }
-}
-```
-Valid types:
-- any
-- string
-- number
-- boolean
-- array
-- object (defined as above)
 
 ## Logging and Debugging
 
-There is a static property in the Table class to set the level of logging for all tables, you can set like this:
-
-```
-Table.loglevel = LogLevel.None
-```
-The levels are:
-- **None** (default)
-- **Init** (shows table initialisation info)
-- **Operations** (shows table operations)
-- **Verbose** (lots of info for debugging)
--- src/session-manager.ts
+++ src/session-manager.ts
@@ -1,3 +1,103 @@
+import { createClient, type RedisClientType } from "redis";
+import { v4 as uuid } from "uuid";
+
+export interface Session {
+  v: string;
+  lastUsed: number;
+  data: any;
+}
+
 export class SessionManager {
-  
+
+  version: string;
+  redis: RedisClientType;
+
+
+  constructor(redisUrl: string, redisDbIndex: number | string, version: string) {
+    this.version = version;
+    this.redis = createClient({ url: redisUrl });
+    this.redis.on('error', (err) => console.log('Redis Client Error', err));
+    this.redis.connect().then(() => {
+      this.redis.select(Number(redisDbIndex));
+    });
+  }
+
+  async getSession(id: string) {
+    if (!id) return null;
+    let session = await this.redis.get(id);
+    return session
+      ? JSON.parse(session)
+      : null
+  }
+
+  async newSession(data: any = {}): Promise<[string, Session]> {
+    let id = uuid();
+    let session: Session = {
+      v: this.version,
+      lastUsed: Date.now(),
+      data: data
+    };
+    return [id, session];
+  }
+
+  async saveSession(id: string, session: Session) {
+    await this.redis.set(id, JSON.stringify(session));
+  }
+
+  async getSessionCount() {
+    return await this.redis.dbSize();
+  }
+
+  async getAllSessions() {
+    let COUNT = 2000
+    let maxKeys = 2000
+    let curser = 0
+    let res = await this.redis.scan(0, { COUNT })
+    let keys = [...res.keys]
+    while (res.cursor != 0 && keys.length <= maxKeys) {
+      curser = res.cursor
+      keys = [...keys, ...res.keys]
+      res = await this.redis.scan(curser, { COUNT })
+    }
+    keys = keys.slice(0, maxKeys)
+    let sessionData = await Promise.all(keys.map(async (v) => {
+      return await this.redis.get(v)
+    }))
+    let sessions = keys.map((v, i) => {
+      return { id: v, data: sessionData[i] }
+    })
+    return sessions
+  }
+
+  async removeSession(id: string) {
+    return await this.redis.del([id])
+  }
+
+  async removeSessionsOlderThan(millis: number) {
+    const sessions = await this.getAllSessions();
+    let clearedCount = 0
+    for (let s of sessions) {
+      try {
+        let data = JSON.parse(s.data!) as Session
+        if (!Object.hasOwn(data, 'user')) {
+          clearedCount++;
+          this.removeSession(s.id);
+          continue;
+        }
+        if (data.v !== this.version) {
+          clearedCount++;
+          this.removeSession(s.id);
+          continue;
+        }
+        if (data.lastUsed < Date.now() - millis) {
+          clearedCount++;
+          this.removeSession(s.id);
+          continue;
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    console.log(`Cleared ${clearedCount} sessions`)
+  }
 }
